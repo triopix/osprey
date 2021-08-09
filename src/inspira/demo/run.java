3c360036063a7451da2625d408f26ca49ff42269
Archi991-dot <archismannath380@gmail.com>
2021-08-08 23:20:47 -0400

    Testing constants - update 1.0.0

--- src/inspira/demo/run.java
+++ src/inspira/demo/run.java
@@ -1,10 +1,17 @@
 package inspira.demo;
 
 import inspira.basic.BasicOperations;
+// import inspira.constants.MathConstants;
 
 public class run {
     public static void main(String[] args) {
         double basicTest = BasicOperations.divide(30, 5);
         System.out.println(basicTest);
+
+        //testing constants
+
+//        System.out.println(MathConstants.UNITS_SQUARED);
+//        System.out.println(MathConstants.PI);
+//        System.out.println(MathConstants.E);
     }
 }
