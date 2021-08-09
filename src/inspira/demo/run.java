f11d0baa31f178fa9318d08e7a1138e59d0eafab
Archi991-dot <archismannath380@gmail.com>
2021-08-08 23:15:10 -0400

    Update 1.0.0 - organized packages - completed BasicOperations.java - methods

---
+++ src/inspira/demo/run.java
@@ -0,0 +1,10 @@
+package inspira.demo;
+
+import inspira.basic.BasicOperations;
+
+public class run {
+    public static void main(String[] args) {
+        double basicTest = BasicOperations.divide(30, 5);
+        System.out.println(basicTest);
+    }
+}
