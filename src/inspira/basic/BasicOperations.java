544dc5e463bc6e0737424e0ffd587e9036bc2b74
Archi991-dot <archismannath380@gmail.com>
2021-08-08 23:14:08 -0400

    Update 1.0.0 - organized packages - completed BasicOperations.java - methods

---
+++ src/inspira/basic/BasicOperations.java
@@ -0,0 +1,28 @@
+package inspira.basic;
+
+public class BasicOperations {
+
+    public static double add(double[] nums) {
+        double sum = 0;
+        for (double num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double[] nums) {
+        int product=1;
+        for(double k : nums) {
+            product *= k;
+        }
+        return product;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+}
