0859adec22c2667b146fd487f22153ccb6f1b4e3
Archi991-dot <archismannath380@gmail.com>
2021-08-19 12:06:18 -0400

    Added a few more packages

---
+++ src/osprey/compsci/OspreyAlgorithms.java
@@ -0,0 +1,7 @@
+package osprey.compsci;
+
+public class OspreyAlgorithms {
+
+    //searching
+    //sorting algorithms will go here
+}
---
+++ src/osprey/phymath/MathDynamics.java
@@ -0,0 +1,6 @@
+package osprey.phymath;
+
+public class MathDynamics {
+
+    //speed velocity distance time and acceleration formulas will go here
+}
