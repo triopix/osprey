f5466f6bf8067356d530d8f24c0d77df4860ba7d
Archisman Nath <61641517+Nebulax418@users.noreply.github.com>
2021-09-23 16:06:09 -0400

    Update OspreyUtils.java

--- src/utils/OspreyUtils.java
+++ src/utils/OspreyUtils.java
@@ -6,7 +6,7 @@ public final class OspreyUtils {
 
     private OspreyUtils() {};
 
-    //for printing arrays - which is really a pain in the ass
+    //for printing arrays - which is really a pain
     public static String printArray(double[] array) {
         return Arrays.toString(array);
     }
